5fe8aa5de3b534a2b4ba05c2617e1298039753d7
navarrs <ingrid.navarro.an@gmail.com>
2020-09-04 14:05:55 -0500

    update hw1 - solves Ax = b, simple case

--- hw1/q1.py
+++ hw1/q1.py
@@ -6,16 +6,28 @@
 #                                     Ax = b 
 
 #
-# Includes --------------------------------------------------------------------
+# Includes ---------------------------------------------------------------------
 import numpy as np
 from numpy.linalg import matrix_rank, inv
 
 #
 # Function implementation ------------------------------------------------------
-def DecomposeLA(A):
-  assert A.shape[0] == A.shape[1] and matrix_rank(A) == A.shape[0], \
-    "Input matrix is singular"
+def DecomposeLDU(A):
+  """
+    Performs LDU decomposition via Gaussian reduction.
 
+    Inputs
+    ------
+      A: Square (mxn, n=m), non-singular matrix that will be decomposed
+
+    Outputs
+    -------
+      L: Low triangular matrix (mxm)
+      D: Diagonal matrix (mxm)
+      U: Upper triangular matrix (mxn, n=m)
+  """
+  assert A.shape[0] == A.shape[1] and matrix_rank(A) == A.shape[0], \
+    "Input matrix is invalid"
   L = np.identity(A.shape[0])
   D = np.identity(A.shape[0])
   U = np.identity(A.shape[0])
@@ -37,18 +49,61 @@ def DecomposeLA(A):
   
   U = inv(D) * A
   return L, D, U
-    
-    
 
+def Solve(A, b):
+  """
+    Solves an equation of the form Ax = b via LDU decomposition. This is done in 
+    three main steps:
+      (1) Get L, D, U matrices
+      (2) Solve for y: Ly = b'
+      (3) Sove for x: Ux = Dinv * y
 
+    Inputs
+    ------
+      A: Square (mxn, n=m), non-singular matrix that will be decomposed
+      b: Vector (nx1)
+
+    Outputs
+    -------
+      x: Vector (nx1) that solves Ax = b
+  """
+  # Decompose matrix A
+  L, D, U = DecomposeLDU(A)
+  #print("L: ", L, "\nD: ", D, "\nU: ", U)
+  
+  x = np.zeros(A.shape[0])
+  y = np.zeros(A.shape[0])
+
+  # Solve Ly = b' using forward substitution, e.g:
+  # ( 1  0  0 ) (y1)   (b1')        y1 = b1'
+  # ( a  1  0 ) (y2) = (b2')  --->  y2 = b2' - a * y1
+  # ( b  c  1 ) (y3)   (b3')        y3 = b3' - b * y1 - c * y3
+  for i in range(y.shape[0]):
+    y[i] = b[i]
+    for j in range(i):
+      y[i] -= L[i, j] * y[j]
+  # print("y: ", y)
+  
+  # Solve Ux = D^-1 * y using backward substitution
+  # ( 1  a  b ) (x1)   (y1')        x1 = y1' - a * x1 - b * x3
+  # ( 0  1  c ) (x2) = (y2')  --->  x2 = y2' - c * x1
+  # ( 0  0  1 ) (x3)   (y3')        x3 = y3' 
+  y = inv(D).dot(y)
+  for i in reversed(range(y.shape[0])):
+    x[i] = y[i]
+    for j in reversed(range(i)):
+      x[i] -= U[i, j] * x[i]
+  #print("x: ", x)
+
+  return x
 
 #
 # Main program -----------------------------------------------------------------
 def main():
   #A = np.random.rand(3, 3)
   A = np.matrix([[1, -2, 1], [1, 2, 2], [2, 3, 4]], dtype=float)
-  L, D, U = DecomposeLA(A)
-  print("L: ", L, "\nD: ", D, "\nU: ", U)
+  b = np.array([5, 9, 2], dtype=float)
+  x = Solve(A, b)
 
 if __name__ == "__main__":
   main()
