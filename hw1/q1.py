d0b03b8811291d9eb0ba1ec6f478ced7c51454dc
navarrs <ingrid.navarro.an@gmail.com>
2020-09-06 21:16:12 -0500

    update hw1 - solves Ax = b with P

--- hw1/q1.py
+++ hw1/q1.py
@@ -2,11 +2,12 @@
 # @andrewid   ingridn
 # @date       Sept 4, 2020
 #
-# @brief      Implementation of the LDU decomposition to solve the equation  
-#                                     Ax = b 
+# @brief      Implementation of the LDU decomposition to solve the equation  #   
+#                                   Ax = b 
 
 #
 # Includes ---------------------------------------------------------------------
+import argparse
 import numpy as np
 from numpy.linalg import matrix_rank, inv
 
@@ -15,95 +16,122 @@ from numpy.linalg import matrix_rank, inv
 def DecomposeLDU(A):
   """
     Performs LDU decomposition via Gaussian reduction.
-
     Inputs
     ------
       A: Square (mxn, n=m), non-singular matrix that will be decomposed
-
     Outputs
     -------
       L: Low triangular matrix (mxm)
       D: Diagonal matrix (mxm)
       U: Upper triangular matrix (mxn, n=m)
+      P: Permutation matrix (mxm)
   """
   assert A.shape[0] == A.shape[1] and matrix_rank(A) == A.shape[0], \
     "Input matrix is invalid"
-  L = np.identity(A.shape[0])
-  D = np.identity(A.shape[0])
-  U = np.identity(A.shape[0])
 
-  for i in range(A.shape[0]):
-    pivot = A[i, i]
+  n = A.shape[0]
+  P = np.identity(n)
+  L = np.identity(n)
+  D = np.identity(n)
+  U = np.identity(n)
 
-    # Get L values below diagonal for column i
-    for j in range(i+1, A.shape[1]):
-      L[j, i] = A[j, i] / pivot
-
-      # Update jth A row as: 
-      #    rowj = rowj - Lji * rowi
-      for k in range(i, A.shape[0]):
-        A[j, k] -= L[j, i] * A[i, k]
-    
-    # Update diagonal value dii 
-    D[i, i] = A[i, i]
+  for i in range(n):
+    # Identify if a permutation is required
+    # q: scaled values in the ith column 
+    q = [0.0 for i in range(i, n)]
+    idx = 0
+    for j in range(i, n):
+      # s: max values per row (from i to n)
+      s = 0.0
+      for k in range(i, n):
+        v = abs(A[j, k])
+        s = v if v > s else s
+      q[idx] = abs(A[j, i]) / s
+      idx += 1
+    max_i = q.index(max(q)) + i
   
+    # Permute if max_i is not equal to current row
+    if max_i != i:
+      A[[max_i, i]] = A[[i, max_i]]
+      P[[max_i, i]] = P[[i, max_i]]
+    
+    # Gaussian reduction on A
+    for j in range(i+1, n):
+      # pivot
+      A[j, i] /= A[i, i]
+      # Update jth A row as: 
+      #    rowj = rowj - pivot * rowi
+      for k in range(i+1, n):
+        A[j, k] -=  A[j, i] * A[i, k]
+
+  # Decompose L and D
+  for i in range(n):
+    for j in range(n):
+      if i == j:
+        D[i, i] = A[i, j]
+      elif i > j:
+        L[i, j], A[i, j] = A[i, j], 0.0
+
+  # Decompose DU
   U = inv(D) * A
-  return L, D, U
+  return L, D, U, P
 
 def Solve(A, b):
   """
     Solves an equation of the form Ax = b via LDU decomposition. This is done in 
     three main steps:
-      (1) Get L, D, U matrices
+      (1) Get L, D, U, P matrices
       (2) Solve for y: Ly = b'
       (3) Sove for x: Ux = Dinv * y
-
     Inputs
     ------
       A: Square (mxn, n=m), non-singular matrix that will be decomposed
       b: Vector (nx1)
-
     Outputs
     -------
       x: Vector (nx1) that solves Ax = b
   """
   # Decompose matrix A
-  L, D, U = DecomposeLDU(A)
-  #print("L: ", L, "\nD: ", D, "\nU: ", U)
+  n = A.shape[0]
+  A_ = A.copy()
+  L, D, U, P = DecomposeLDU(A_)
   
-  x = np.zeros(A.shape[0])
-  y = np.zeros(A.shape[0])
+  x = np.zeros(n)
+  y = np.zeros(n)
 
   # Solve Ly = b' using forward substitution, e.g:
   # ( 1  0  0 ) (y1)   (b1')        y1 = b1'
   # ( a  1  0 ) (y2) = (b2')  --->  y2 = b2' - a * y1
   # ( b  c  1 ) (y3)   (b3')        y3 = b3' - b * y1 - c * y3
-  for i in range(y.shape[0]):
+  b = P.dot(b)
+  for i in range(n):
     y[i] = b[i]
     for j in range(i):
       y[i] -= L[i, j] * y[j]
-  # print("y: ", y)
   
-  # Solve Ux = D^-1 * y using backward substitution
+  # Solve Ux = D^-1 * y using back substitution
   # ( 1  a  b ) (x1)   (y1')        x1 = y1' - a * x1 - b * x3
   # ( 0  1  c ) (x2) = (y2')  --->  x2 = y2' - c * x1
   # ( 0  0  1 ) (x3)   (y3')        x3 = y3' 
   y = inv(D).dot(y)
-  for i in reversed(range(y.shape[0])):
+  for i in reversed(range(n)):
     x[i] = y[i]
-    for j in reversed(range(i)):
-      x[i] -= U[i, j] * x[i]
-  #print("x: ", x)
-
+    for j in range(n-1, i, -1):
+      x[i] -= U[i, j] * x[j]
   return x
 
 #
 # Main program -----------------------------------------------------------------
-def main():
-  #A = np.random.rand(3, 3)
-  A = np.matrix([[1, -2, 1], [1, 2, 2], [2, 3, 4]], dtype=float)
-  b = np.array([5, 9, 2], dtype=float)
+def main(size, min_val, max_val):
+  A = np.matrix(np.random.uniform(low=min_val, high=max_val, size=(size, size)))
+  b = np.matrix(np.random.uniform(low=min_val, high=max_val, size=(size, 1)))
   x = Solve(A, b)
-
+  print("x: ", x)
+  
 if __name__ == "__main__":
-  main()
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--size", help="matrix size", type=int, default=5)
+  parser.add_argument("--min_val", help="min value in matrix", type=float, default=-100.0)
+  parser.add_argument("--max_val", help="max value in matrix", type=float, default=100.0)
+  args = parser.parse_args()
+  main(args.size, args.min_val, args.max_val)
---
+++ hw1/utest.py
@@ -0,0 +1,53 @@
+# @author     Ingrid Navarro 
+# @andrewid   ingridn
+# @date       Sept 4, 2020
+#
+# @brief      Unit Test for q1.py
+
+import argparse
+import numpy as np
+import q1
+
+class TestLDU():
+  def __init__(self, n_min=2, n_max=100, low=-10000.0, high=10000.0):
+    # Max matrix size
+    self.n_min = n_min
+    # Max matrix size
+    self.n_max = n_max
+    # Min matrix value
+    self.low = low
+    # Max matrix value
+    self.high = high
+
+  def TestDecompositionLDU(self):
+    n = np.random.randint(low=self.n_min, high=self.n_max)
+    print("Testing q1.DecompositionLDU with Matrix of size {}x{}".format(n, n))
+    A = np.matrix(np.random.uniform(low=self.low, high=self.high, size=(n, n)))
+    A_ = A.copy()
+    L, D, U, P = q1.DecomposeLDU(A)
+    PA = P.dot(A_)
+    LDU = (L.dot(D)).dot(U)
+    assert np.allclose(PA, LDU), "PA != LDU"
+  
+  def TestSolve(self):
+    n = np.random.randint(low=self.n_min, high=self.n_max)
+    print("Testing q1.Solve with n = {}".format(n))
+    A = np.matrix(np.random.uniform(low=self.low, high=self.high, size=(n, n)))
+    A_ = A.copy()
+    b = np.matrix(np.random.uniform(low=self.low, high=self.high, size=(n, 1)))
+    b_ = b.copy()
+    x = q1.Solve(A, b)
+    assert np.allclose(A_.dot(x), np.transpose(b_)), "Ax != b"
+    
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--n_tests", help="tests to execute", type=int, default=10)
+  args = parser.parse_args()
+
+  test = TestLDU()
+
+  for t in range(args.n_tests):
+    print("Test {}".format(t))
+    test.TestDecompositionLDU()
+    test.TestSolve()
+  print("All tests passed")
