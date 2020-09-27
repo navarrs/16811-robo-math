b8fe5198adeaffb3bbaea70aa4af55325750ba1b
navarrs <ingrid.navarro.an@gmail.com>
2020-09-27 13:26:31 -0500

    hw2 - q1 tests

--- hw2/code/q1.py
+++ hw2/code/q1.py
@@ -2,8 +2,7 @@
 # @andrewid   ingridn
 # @date       Sept 24, 2020
 #
-# @brief      Polynomial interpolation using Newton's divided differences. 
-# @notes     
+# @brief      Polynomial interpolation using divided differences approach.       
 
 #
 # Includes ---------------------------------------------------------------------
@@ -15,7 +14,7 @@ import numpy as np
 # Problem implementation -------------------------------------------------------
 def ComputeDivDiff(X, Y):
   """
-    Computes the divided difference table.
+    Computes the divided difference coefficients. 
     Inputs
     ------
       X: an (N, 1) array of values
@@ -27,7 +26,7 @@ def ComputeDivDiff(X, Y):
   assert X.shape == Y.shape, \
     f"Shapes of X:{X.shape} and Y:{Y.shape} don't match"
   N = X.shape[0]
-  dd = Y
+  dd = Y.copy()
   # Compute the divided differences 
   for i in range(1, N):
     for j in range(N-1, i-1, -1):
@@ -43,40 +42,73 @@ def ComputeDivDiff(X, Y):
   return dd
 
 def InterpolateValue(X, Y, x):
+  """
+    Interpolates a value at x as:
+      pn(x) = A0 + A1(x- x0) + A2(x - x0)(x - x1) + ... + An(x - x0)...(x - xn)
+    using divided differences to obtain the coefficients Ai (i=0,...,n)
+    Inputs
+    ------
+      X        : (N, 1) array with x values
+      Y        : (N, 1) array with y values
+      x_test   : Value at which we want to estimate f(x)
+    Outputs
+    -------
+      y_interp : Estimated f(x) at x
+  """
   dd = ComputeDivDiff(X, Y)
   N = dd.shape[0]
-  value = dd[0]
+  y_interp = dd[0]
   # print(dd)
   for i in range(1, N):
     a = dd[i]
     for j in reversed(range(i)):
       a *= (x - X[j])
-    value += a
-  return value
+    y_interp += a
+  return y_interp
     
 #
 # Helper methods ---------------------------------------------------------------
-def CosPi(x, eps=0.00001):
-  c = cos(np.pi * x)
+def CosPi(x, eps=0.0000001):
+  """ f(x) from question 1 (b) """
+  c = np.cos(np.pi * x)
   return c if c > eps else 0.0
 
 def ComputeX(n):
+  """ xi from question 1 (c) and (d) """
   return np.asarray([(i * 2 / n) - 1 for i in range(n)])
 
 def ComputeF(x):
-  return 2 / (1 + 9 * (x **2))
+  """ f(x) from question 1 (c) and (d) """
+  return 2 / (1 + 9*(x**2))
 
-def Display(X, Y, x, y_interp, y_truth = None, err = None, diplay_all=True):
-  print(f"--------------------------------------------------------------------")
+def Test(X, Y, x_test, y_truth = None, display_all=True):
+  """
+    Estimates f(x) at x_test given X, Y.
+    Inputs
+    ------
+      X       : (N, 1) array with x values
+      Y       : (N, 1) array with y values
+      x_test  : Value at which we want to estimate f(x)
+      y_truth : Actual value of f(x) 
+    Outputs
+    -------
+      err     : Error value between f(x) and p(x) 
+  """
+  err = None
   print(f"X: {X}\nY: {Y}")
-  if diplay_all:
+  
+  y_interp = InterpolateValue(X, Y, x_test)
+  
+  # Compute error if y_truth was given
+  if np.all(y_truth) != None:
+    err = np.abs(y_truth - y_interp)
+    
+  if display_all:
     print(f"Interpolate at x:\n{x}")
     print(f"Interpolation result y_interp:\n{y_interp}")
-    if np.all(y_truth) != None:
-      print(f"y_truth: {y_truth}")
+    print(f"y_truth:\n{y_truth}\nerror: {err}")
   
-  if err != None:
-    print(f"error: {err}")
+  return err
   
   
 #
@@ -84,52 +116,48 @@ def Display(X, Y, x, y_interp, y_truth = None, err = None, diplay_all=True):
 if __name__ == "__main__":
   
   # Q1.A
-  # print(f"Question 1 (a)")
-  # X = np.array([5.0, 6.0, 9.0, 11.0], dtype=np.float32)
-  # Y = np.array([12.0, 13.0, 14.0, 16.0], dtype=np.float32)
-  # x = 7
-  # y_interp = InterpolateValue(X, Y, x)
-  # Display(X, Y, x, y_interp)
-
-  # X = np.array([0.0, 1.0, -1.0], dtype=np.float32)
-  # Y = np.array([1.0, 0.0, 4.0], dtype=np.float32)
-  # x = 2.0
-  # y_interp = InterpolateValue(X, Y, x)
-  # Display(X, Y, x, y_interp)
+  print(f"\n------------------------------------------------------------------")
+  print(f"Question 1 (a) - Interpolating f(x) = (x - 1)^2")
+  # Interpolating f(x) = (x - 1)^2
+  X = np.array([0.0, 1.0, -1.0], dtype=np.float32)
+  Y = np.array([1.0, 0.0, 4.0], dtype=np.float32)
+  x = -3
+  Test(X, Y, x)
   
   # Q1.B
-  # print(f"Question 1 (b)")
-  # x = 3 / 10
-  # X = np.array([0, 1/8, 1/4, 3/8, 1/2], dtype=np.float32)
-  # Y = np.asarray([CosPi(x) for x in X], dtype=np.float32)
-  # y_interp = InterpolateValue(X, Y, x)
-  # y_truth = CosPi(x)
-  # Display(X, Y, x, y_interp, y_truth)
+  print(f"\n------------------------------------------------------------------")
+  print(f"Question 1 (b) - Interpolating f(x) = cos(pi*x)")
+  x = 3 / 10
+  y_truth = CosPi(x)
+  X = np.array([0, 1/8, 1/4, 3/8, 1/2], dtype=np.float32)
+  Y = np.asarray([CosPi(x) for x in X], dtype=np.float32)
+  Test(X, Y, x, y_truth)
+
+  # Q1.C 
+  print(f"\n------------------------------------------------------------------")
+  print(f"Question 1 (c) - Interpolating f(x) = 2 / (1 + 9x^2)")
+  x = 0.07
+  y_truth = ComputeF(x)
+  N = [2, 4, 40]
+  for n in N: 
+    print(f"\n*** With n: {n}")
+    X = ComputeX(n)
+    Y = ComputeF(X)
+    Test(X, Y, x, y_truth)
   
-  # (c)
-  # print(f"Question 1 (c)")
-  # x = 0.07
-  # N = [2, 4, 40]
-  # for n in N:
-  #   print(f"\nWith n: {n}")
-  #   X = ComputeX(n)
-  #   Y = ComputeF(X)
-  #   y_interp = InterpolateValue(X, Y, x)
-  #   y_truth = ComputeF(x)
-  #   Display(X, Y, x, y_interp, y_truth)
-  
-  # (d)
-  print(f"Question 1 (d)")
+  # Q1.D 
+  print(f"\n------------------------------------------------------------------")
+  print(f"Question 1 (d) - Estimating max error of f(x) = 2 / (1 + 9x^2)")
   N = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 40]
-  En = np.zeros((len(N), 1), dtype=np.float32) 
+  En = np.zeros((len(N), 1), dtype=np.float32)
   delta = 0.01
   x = np.arange(-1, 1, delta)
   for i, n in enumerate(N):
-    print(f"\nWith n: {n} and interval discrete size {delta}")
+    print(f"\n*** With n: {n}")
     X = ComputeX(n)
     Y = ComputeF(X)
+    # y_truth
     fx = ComputeF(x)
-    px = InterpolateValue(X, Y, x)
-    err = np.abs(fx - px)
+    err = Test(X, Y, x, fx, display_all=False)
     En[i] = err[np.argmax(err)]
-    Display(X, Y, x, px, fx, En[i], diplay_all=False)
+    print(f"*** Max error: {En[i]}")
